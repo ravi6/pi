3a8698f0cac371306ebfd9130c2cf8d956b4a95c
ravi6 <ravi@hello.com>
2023-12-02 19:58:37 +1100

    About to disable real I2C calls

--- devs/pid.c
+++ devs/pid.c
@@ -6,6 +6,7 @@
 #include "i2c.h"
 #include "mcp3424.h"
 #include "mcp4725.h"
+#include <sys/file.h>
 
 struct PID {
   float mv     ; // Measured Value 
@@ -14,19 +15,13 @@ struct PID {
   float Kc     ; // Gain
   float Ki     ; // Integral action (s)
   float Kd     ; // Derivative action 
-  float mvMax  ; // Measured value limits
-  float mvMin  ;
-  float opMax  ; // Output limits
+  float mvMin  ; 
+  float mvMax  ; // Measured value (pressure) limits
   float opMin  ;
+  float opMax  ;  // Output (flow setpoint) limits
+  int   cmd    ; // 1 start, 0 stop
+  int   state  ; // 1 running, 0 stopped
 } pid ;
-
-float getSetPt() { // let us use another anlog signal for this 
-   //return ( MCP3424_AtoD(1) ) ;    // using Ch1 for this 
-   // Fixed set point for now
-   float setpt ;
-   setpt = 0.5 ;   //  pressure in bar  
-   return (setpt);
-} // end getSetPt
 
 float getP() {
    float v, vmin, vmax ;
@@ -59,7 +54,32 @@ void setFlow (float f) {
    MCP4725_DtoA(v) ;
 }
 
-int pidLoop(float sp, float kc, float ki, float kd) {
+void readParams(int wait) {
+  // Read pidParams, command  from parrams file
+  // don't wait if file is busy and lock it when reading from it
+   FILE *fp ;
+
+back:
+   fp = fopen("pidParams", "r") ;
+   if ((fp == NULL)  && !wait) return ;
+   else if ((fp == NULL) && wait) goto back  ;
+
+   float kc, ki, kd ;
+   int cmd ;
+   int n = fscanf(fp, "%d %f %f %f", &cmd, &kc, &ki, &kd); 
+   fclose(fp) ;
+
+   pid.opMin = 0 ;    //flow
+   pid.opMax = 100 ;
+   pid.mvMin = 0 ;   // pressure
+   pid.mvMax = 1 ;
+   pid.Kc =  kc *  (pid.opMax - pid.opMin) / (pid.mvMax - pid.mvMin) ;
+   pid.Ki = ki ;
+   pid.Kd = kd ;
+   pid.cmd = cmd ;
+} // end readParams
+
+int pidLoop() {
 
   float err, err_p  ;   // (SP - MV)  in the current and last time step
   float errInt ;  // Integral of Error w.r.t time
@@ -67,24 +87,22 @@ int pidLoop(float sp, float kc, float ki, float kd) {
   float errDer ;  // Time derivative of error
 
   const float  dt = 100e-3   ;   // time interval (s)
-  pid.opMin = 0 ;    //flow
-  pid.opMax = 100 ;
-  pid.mvMin = 0 ;   // pressure
-  pid.mvMax = 1 ;
-  pid.Kc =  kc *  (pid.opMax - pid.opMin) / (pid.mvMax - pid.mvMin) ;
-  pid.Ki = ki ;
-  pid.Kd = kd ;
-
+  int ticks = 0 ;
  
   I2C_Open() ;  // open the bus
 
-  errInt = 0 ;
-  //err_p =  getSetPt() - getP()  ;  // previous error
-  err_p =  sp - getP()  ;  // previous error
+  // Keep monitoring params until pid is started
+  do { readParams(1) ; // load PID params from params file
+		     // wait for ever for the file to be ready
+  } while (pid.cmd == 0) ;
 
-  while(1) {
+  errInt = 0 ;
+  err_p =  pid.sp - getP()  ;  // previous error
+  
+  while(pid.cmd == 1) {
+    if ((ticks%100) == 0)
+     readParams(0) ;  // don't wait if you can't get access to file
      pid.mv = getP() ;
-     pid.sp = sp ;   //getSetPt() ;
      err = pid.sp - pid.mv ;
      errDer = (err - err_p) / dt ;
      errInt = errInt + (err + err_p) * 0.5 * dt ;
@@ -104,79 +122,25 @@ int pidLoop(float sp, float kc, float ki, float kd) {
          pid.op = 0 ;
      //printf("mv = %f err= %f op= %f\n", pid.mv, err, pid.op); 
      //printf("errInt = %f err_p= %f \n", errInt, err_p); 
-     printf("%f  %f %f \n", pid.sp, pid.mv, pid.op) ;
-     
      setFlow(pid.op) ;
+
+     
+     FILE *fp = fopen("pidData","rw") ;
+     if (fp != NULL) {
+       printf("%d %f  %f %f \n", ticks, pid.sp, pid.mv, pid.op) ;
+       fclose(fp) ;
+     }
+
      err_p = err ;    // ready for next loop
      usleep(dt*1e6) ;  // usleep works in microseconds
+     ticks = ticks + 1 ;
   } // PID loop end
 
+  pid.state = 0 ;
 } // end PID controller  loop
 
-
-void testAtoD() {
-
-// Test AtoD 
-     I2C_Open() ;  // open the bus
-     I2C_setSlave(MCP3424_I2C_ADD) ;  // Talkto A2D
-     printf("Using Device Add %x \n", MCP3424_I2C_ADD);
-     MCP3424_reset() ;
-     int channel = 3 ;
-     printf("Read Channel %d\n", channel) ;
-     float vin ;
-     vin = MCP3424_AtoD(channel) ; // read from ch0
-     printf("Channel:%d Voltage:  %6.3f \n", channel, vin) ;
-     float bar ;
-     float VDD ;
-     VDD = 5.2 ;
-     bar = ( ( vin  / 0.196 ) - 0.1 * VDD) / ( 0.8 * VDD);
-     printf("Pressure in Bar : %f \n ", bar) ;
-}
-
-void testDtoA() {
-// Test DtoA 
-     I2C_Open() ;  // open the bus
-     I2C_setSlave(MCP4725_I2C_ADD) ;  // Talkto A2D
-     printf("Using MCP4725 Device Add %x \n", MCP4725_I2C_ADD);
- //    MCP4725_reset() ;
-     float vout ;
-     printf("Enter Volts:  ");
-     scanf("%f", &vout) ;
-     printf("Vout should be %f\n", vout);
-     MCP4725_DtoA(vout) ; // 
-}
-int testAll() {
-     I2C_Open() ;  // open the bus
-     I2C_setSlave(MCP4725_I2C_ADD) ;  // Talkto D2A
-     printf("Using MCP4725 Device Add %x \n", MCP4725_I2C_ADD);
-     MCP4725_reset() ;
-
-     I2C_setSlave(MCP3424_I2C_ADD) ;  // Talkto A2D
-     printf("Using MCP3424 Device Add %x \n", MCP3424_I2C_ADD);
-     MCP3424_reset() ;
-
-     float x[6] = {0.111, 0.222, 0.333, 0.444, 0.555, 5.0} ;
-     float vout ;
-     for (int j=0 ; j < 6 ; j++) {
-       I2C_setSlave(MCP4725_I2C_ADD) ;  // Talkto D2A
-       vout = x[j] ;
-       I2C_setSlave(MCP4725_I2C_ADD) ;  
-       MCP4725_DtoA(vout) ; // 
-       int channel = 3 ;
-       float vin ;
-       I2C_setSlave(MCP3424_I2C_ADD) ;  // Talkto A2D
-       vin = MCP3424_AtoD(channel) ; // read from ch0
-       printf("Channel:%d Voltage:  %6.3f  %6.3f \n",  channel, vin, vout) ;
-    }
-}
 int main(){
   float sp, kc, ki, kd ;
-   sp = 0.8 ;
-   kc = 1.6 ;
-   ki = 0.1 ;
-   kd = 0 ;    
-//   testDtoA() ;
-//   testAtoD() ;
- //    testAll() ;
-    pidLoop(sp, kc, ki, kd) ;
+ //  sp = 0.8 ; kc = 1.6 ; ki = 0.1 ; kd = 0 ;    
+  pidLoop(sp, kc, ki, kd) ;
 }
